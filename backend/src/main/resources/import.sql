e4b08512da03b33521f0650bc3cbf6bce03a6aa2
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-08-06 11:48:02 -0300

    Create database seed categories

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- with time example
+-- INSERT INTO tb_category (name, created_At) VALUES ('Móveis', NOW());
+INSERT INTO tb_category (name) VALUES ('Livros');
+INSERT INTO tb_category (name) VALUES ('Eletrônicos');
+INSERT INTO tb_category (name) VALUES ('Computadores');
