9a87273a74f8318b8a634645c93932bdd6e593bf
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-08-05 23:04:34 -0300

    Project created

---
+++ backend/src/main/java/com/devjoliveira/jocatalog/JocatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.jocatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JocatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JocatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devjoliveira/jocatalog/JocatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.jocatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JocatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
