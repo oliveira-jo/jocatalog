e1c2c38afc866d508958008f45e51b8ea0cd5a8d
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-08-06 11:41:19 -0300

    Create category controller

---
+++ backend/src/main/java/com/devjoliveira/jocatalog/controllers/CategoryController.java
@@ -0,0 +1,53 @@
+package com.devjoliveira.jocatalog.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devjoliveira.jocatalog.dtos.CategoryDTO;
+import com.devjoliveira.jocatalog.services.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+  private final CategoryService categoryService;
+
+  public CategoryController(CategoryService categoryService) {
+    this.categoryService = categoryService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<CategoryDTO>> findAll() {
+    return ResponseEntity.ok().body(categoryService.findAll());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+    return ResponseEntity.ok().body(categoryService.findById(id));
+  }
+
+  @PostMapping
+  public ResponseEntity<CategoryDTO> save(@RequestBody CategoryDTO categoryDTO) {
+    return ResponseEntity.ok().body(categoryService.save(categoryDTO));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<CategoryDTO> change(@PathVariable Long id, @RequestBody CategoryDTO categoryDTO) {
+    return ResponseEntity.ok().body(categoryService.update(id, categoryDTO));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    categoryService.delete(id);
+    return ResponseEntity.ok().build();
+  }
+}
