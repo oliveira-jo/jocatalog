5bce98164d9ccc37bb3fcb2815e1519ee3e43a96
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-08-25 21:36:10 -0300

    Mapped products into categories

--- backend/src/main/java/com/devjoliveira/jocatalog/entities/Category.java
+++ backend/src/main/java/com/devjoliveira/jocatalog/entities/Category.java
@@ -1,12 +1,15 @@
 package com.devjoliveira.jocatalog.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -25,6 +28,9 @@ public class Category {
 
   @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
   private Instant updatedAt;
+
+  @ManyToMany(mappedBy = "categories")
+  private Set<Product> products = new HashSet<>();
 
   public Category() {
   }
@@ -66,6 +72,10 @@ public class Category {
   @PreUpdate
   public void preUpdate() {
     updatedAt = Instant.now();
+  }
+
+  public Set<Product> getProducts() {
+    return this.products;
   }
 
   @Override
